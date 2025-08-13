76e0e88f15a50e22b17edea6e97a7dcbacc1263f
Elia Palme <2025999+theely@users.noreply.github.com>
2025-08-13 17:45:47 +0200

    Update spin.sh

--- auto-spin/src/autospin/scripts/spin.sh
+++ auto-spin/src/autospin/scripts/spin.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name={{job_name}}
 #SBATCH --container-writable
-#SBATCH --environment={{environment}}
 #SBATCH --time={{time_limit}}
 #SBATCH --ntasks-per-node=1
 #SBATCH --dependency=singleton
@@ -9,14 +8,19 @@
 #SBATCH --output={{job_name}}-%j.out
 #SBATCH --error={{job_name}}-%j.err
 
+unset HTTPS_PROXY HTTP_PROXY http_proxy https_proxy no_proxy
+
 export NCCL_SOCKET_IFNAME=lo
 export GLOO_SOCKET_IFNAME=lo
 export PROMETHEUS_MULTIPROC_DIR=/ocfbin/scratch
 export SP_NCCL_SO_PATH=/usr/lib/x86_64-linux-gnu/
-export NO_PROXY=0.0.0.0,127.0.0.1
 
 export MODEL_PATH={{model_path}}
 export PARSER_ARGS="{{model_args}}"
 export MODEL_NAME={{model_name}}
 
-/ocfbin/ocf-v2 start --bootstrap.addr {{bootstrap_addr}} --subprocess "{{sub_process}}" --service.name llm --service.port 8080
+srun -N ${SLURM_JOB_NUM_NODES} --environment={{environment}} --container-writable bash -c '\
+    curl -L "https://github.com/ResearchComputer/OpenComputeFramework/releases/download/v0.1.1/ocf-amd64" > ocf-amd64
+    chmod +x ocf-amd64
+    ./ocf-amd64 start --bootstrap.addr {{bootstrap_addr}} --subprocess "{{sub_process}}" --service.name llm --service.port 8080
+    '
