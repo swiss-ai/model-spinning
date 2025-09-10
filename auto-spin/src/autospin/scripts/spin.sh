5673cc9e76b57e26af4a672938f52ee3b3090181
Elia Palme <2025999+theely@users.noreply.github.com>
2025-09-10 07:42:07 +0200

    Update spin.sh

--- auto-spin/src/autospin/scripts/spin.sh
+++ auto-spin/src/autospin/scripts/spin.sh
@@ -21,10 +21,10 @@ export PARSER_ARGS="{{model_args}}"
 export MODEL_NAME={{model_name}}
 
 
-{% if 'v0.1.1' == ocf_version %}
+{% if 'v0.1.4' == ocf_version %}
  srun -N ${SLURM_JOB_NUM_NODES} --environment={{environment}} --container-writable bash -c '\
     cd /tmp
-    curl -L "https://github.com/ResearchComputer/OpenComputeFramework/releases/download/v0.1.1/ocf-amd64" > ocf-amd64
+    curl -L "https://github.com/ResearchComputer/OpenComputeFramework/releases/download/v0.1.4/ocf-amd64" > ocf-amd64
     chmod +x ocf-amd64
     ./ocf-amd64 start --bootstrap.addr {{bootstrap_addr}} --subprocess "{{sub_process}}" --service.name llm --service.port 8080
     '
